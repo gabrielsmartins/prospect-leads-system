f14ae532dca4de8b57bc8101817f2d10c0c43044
Gabriel <ga.smartins94@gmail.com>
2023-08-09 19:45:00 -0300

    fix: Fix build

--- app/backend/lead-service/configuration/src/test/java/br/pucminas/leads/LeadServiceApplicationTest.java
+++ app/backend/lead-service/configuration/src/test/java/br/pucminas/leads/LeadServiceApplicationTest.java
@@ -1,12 +1,13 @@
 package br.pucminas.leads;
 
+import br.pucminas.leads.adapters.messaging.support.RedisContainerSupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-public class LeadServiceApplicationTest {
+public class LeadServiceApplicationTest extends RedisContainerSupport {
 
     @Test
     void contextLoads() {
