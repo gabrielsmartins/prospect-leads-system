dac338c9d6d22509d95b37b28f574f0b2c6a2f9a
Gabriel <ga.smartins94@gmail.com>
2023-09-12 16:18:08 -0300

    Fix build

--- app/backend/lead-service/adapters/streams/src/test/java/br/pucminas/leads/adapters/streams/support/RedisContainerSupport.java
+++ app/backend/lead-service/adapters/streams/src/test/java/br/pucminas/leads/adapters/streams/support/RedisContainerSupport.java
@@ -1,19 +1,20 @@
 package br.pucminas.leads.adapters.streams.support;
 
 import com.redis.testcontainers.RedisContainer;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class RedisContainerSupport {
 
     private static final RedisContainer REDIS_CONTAINER;
 
     static {
-        REDIS_CONTAINER = new RedisContainer(DockerImageName.parse("redis:7.2.0-alpine")).withExposedPorts(6379)
-                                                                                                      .withReuse(true);
+        REDIS_CONTAINER = new RedisContainer(DockerImageName.parse("redis:7.2.0-alpine")).withExposedPorts(6379);
         REDIS_CONTAINER.start();
     }
 
