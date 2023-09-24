71eb9fed02be66a40d35cca761d7fb59b515c8f9
Gabriel <ga.smartins94@gmail.com>
2023-09-24 10:03:35 -0300

    Update script

--- app/infra/railway/localstack/aws-scripts/init.sh
+++ app/infra/railway/localstack/aws-scripts/init.sh
@@ -10,5 +10,6 @@ awslocal dynamodb \
     --key-schema \
         AttributeName=Id,KeyType=HASH \
     --billing-mode PAY_PER_REQUEST \
+    --endpoint-url="http://0.0.0.0:4566"
 
 echo "########### DynamoDB ${table} Table was created sucessfully ###########"
