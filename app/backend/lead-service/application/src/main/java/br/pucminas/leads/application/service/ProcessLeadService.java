4795bfc944492988ad7353f5e51aa8661ac54e5f
Gabriel <ga.smartins94@gmail.com>
2023-09-24 21:28:03 -0300

    Update avro

--- app/backend/lead-service/application/src/main/java/br/pucminas/leads/application/service/ProcessLeadService.java
+++ app/backend/lead-service/application/src/main/java/br/pucminas/leads/application/service/ProcessLeadService.java
@@ -1,8 +1,10 @@
 package br.pucminas.leads.application.service;
 
 import br.pucminas.leads.application.domain.Lead;
+import br.pucminas.leads.application.domain.exceptions.InsuranceQuoteNotFoundException;
 import br.pucminas.leads.application.ports.in.ProcessLeadUseCase;
 import br.pucminas.leads.application.ports.out.SaveLeadPort;
+import br.pucminas.leads.application.ports.out.SearchInsuranceQuotePort;
 import br.pucminas.leads.application.ports.out.SearchLeadPort;
 import br.pucminas.leads.application.ports.out.SendLeadPort;
 import br.pucminas.leads.application.service.discount.CompoundDiscountApplier;
@@ -17,6 +19,7 @@ public class ProcessLeadService implements ProcessLeadUseCase {
 
     private final SearchLeadPort searchLeadPort;
     private final CompoundDiscountApplier compoundDiscountApplier;
+    private final SearchInsuranceQuotePort searchInsuranceQuotePort;
     private final SendLeadPort sendLeadPort;
     private final SaveLeadPort saveLeadPort;
 
@@ -28,6 +31,16 @@ public class ProcessLeadService implements ProcessLeadUseCase {
     }
 
     private void process(Lead lead) {
+        var insuranceQuoteId = lead.getInsuranceQuote().getId();
+        var insuranceQuote = this.searchInsuranceQuotePort.findById(insuranceQuoteId)
+                                                          .orElseThrow(() -> new InsuranceQuoteNotFoundException(String.format("Insurance quote %s not found", insuranceQuoteId)));
+        if (insuranceQuote.isFinished()) {
+            lead.setInsuranceQuote(insuranceQuote);
+            lead.setFinished(true);
+            this.saveLeadPort.save(lead);
+            return;
+        }
+        lead.setInsuranceQuote(insuranceQuote);
         this.compoundDiscountApplier.apply(lead.getInsuranceQuote());
         lead.setProcessedAt(LocalDateTime.now());
         this.sendLeadPort.send(lead);
--- app/backend/lead-service/application/src/test/java/br/pucminas/leads/application/service/ProcessLeadServiceTest.java
+++ app/backend/lead-service/application/src/test/java/br/pucminas/leads/application/service/ProcessLeadServiceTest.java
@@ -4,6 +4,7 @@ package br.pucminas.leads.application.service;
 import br.pucminas.leads.application.domain.InsuranceQuote;
 import br.pucminas.leads.application.domain.Lead;
 import br.pucminas.leads.application.ports.out.SaveLeadPort;
+import br.pucminas.leads.application.ports.out.SearchInsuranceQuotePort;
 import br.pucminas.leads.application.ports.out.SearchLeadPort;
 import br.pucminas.leads.application.ports.out.SendLeadPort;
 import br.pucminas.leads.application.service.discount.CompoundDiscountApplier;
@@ -13,7 +14,10 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import static br.pucminas.leads.application.support.InsuranceQuoteSupport.defaultInsuranceQuote;
 import static br.pucminas.leads.application.support.LeadSupport.defaultLead;
 import static org.mockito.Mockito.*;
 
@@ -21,6 +25,7 @@ class ProcessLeadServiceTest {
 
     private ProcessLeadService service;
     private SearchLeadPort searchLeadPort;
+    private SearchInsuranceQuotePort searchInsuranceQuotePort;
     private CompoundDiscountApplier compoundDiscountApplier;
     private SendLeadPort sendLeadPort;
     private SaveLeadPort saveLeadPort;
@@ -28,19 +33,45 @@ class ProcessLeadServiceTest {
     @BeforeEach
     public void setup() {
         this.searchLeadPort = mock(SearchLeadPort.class);
+        this.searchInsuranceQuotePort = mock(SearchInsuranceQuotePort.class);
         this.compoundDiscountApplier = mock(CompoundDiscountApplier.class);
         this.sendLeadPort = mock(SendLeadPort.class);
         this.saveLeadPort = mock(SaveLeadPort.class);
-        this.service = new ProcessLeadService(this.searchLeadPort, this.compoundDiscountApplier, this.sendLeadPort, this.saveLeadPort);
+        this.service = new ProcessLeadService(this.searchLeadPort, this.compoundDiscountApplier, this.searchInsuranceQuotePort, this.sendLeadPort, this.saveLeadPort);
     }
 
     @Test
-    @DisplayName("Given Lead When Process Then Send Lead")
-    public void givenLeadWhenProcessThenSendLead() {
+    @DisplayName("Given Lead When Insurance Quote Is Finished Then Save Lead")
+    public void givenLeadWhenInsuranceQuoteIsFinishedThenSaveLead() {
         var leadOne = defaultLead().build();
         var leadTwo = defaultLead().build();
         var leads = List.of(leadOne, leadTwo);
 
+        var insuranceQuote = defaultInsuranceQuote()
+                                .withFinished(true)
+                                .build();
+
+        when(this.searchInsuranceQuotePort.findById(any(UUID.class))).thenReturn(Optional.of(insuranceQuote));
+        when(this.searchLeadPort.findAllPendingReceivedLessThan(any(LocalDateTime.class))).thenReturn(leads);
+
+        this.service.process();
+        verify(this.compoundDiscountApplier, never()).apply(any(InsuranceQuote.class));
+        verify(this.sendLeadPort, never()).send(any(Lead.class));
+        verify(this.saveLeadPort, times(leads.size())).save(any(Lead.class));
+    }
+
+    @Test
+    @DisplayName("Given Lead When Insurance Quote Is Not Finished Then Send Lead")
+    public void givenLeadWhenInsuranceQuoteIsNotFinishedThenSendLead() {
+        var leadOne = defaultLead().build();
+        var leadTwo = defaultLead().build();
+        var leads = List.of(leadOne, leadTwo);
+
+        var insuranceQuote = defaultInsuranceQuote()
+                                .withFinished(false)
+                                .build();
+
+        when(this.searchInsuranceQuotePort.findById(any(UUID.class))).thenReturn(Optional.of(insuranceQuote));
         when(this.searchLeadPort.findAllPendingReceivedLessThan(any(LocalDateTime.class))).thenReturn(leads);
 
         this.service.process();
