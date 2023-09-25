50a6928b0578529da241b2600942ce2edbf00618
Gabriel <ga.smartins94@gmail.com>
2023-09-25 19:46:41 -0300

    Add Region

--- app/infra/railway/localstack/aws-scripts/ses.sh
+++ app/infra/railway/localstack/aws-scripts/ses.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 email="tccleadspucminas.2023@gmail.com"
 echo "########### Registering e-mail ${email} ###########"
-awslocal ses verify-email-identity --email-address $email
+awslocal ses verify-email-identity --email-address $email --region sa-east-1
 echo "########### E-mail ${email} was registered successfully ###########"
