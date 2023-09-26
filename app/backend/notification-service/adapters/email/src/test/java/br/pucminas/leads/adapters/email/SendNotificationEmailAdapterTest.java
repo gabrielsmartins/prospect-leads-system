f5ff82895a4dd794aa291e6fb22426d8a195f963
Gabriel <ga.smartins94@gmail.com>
2023-09-26 12:45:29 -0300

    Add E-mail template

--- app/backend/notification-service/adapters/email/src/test/java/br/pucminas/leads/adapters/email/SendNotificationEmailAdapterTest.java
+++ app/backend/notification-service/adapters/email/src/test/java/br/pucminas/leads/adapters/email/SendNotificationEmailAdapterTest.java
@@ -1,59 +1,46 @@
 package br.pucminas.leads.adapters.email;
 
+import br.pucminas.leads.adapters.email.config.EmailConfiguration;
 import br.pucminas.leads.adapters.email.config.ObjectMapperConfiguration;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.dumbster.smtp.SimpleSmtpServer;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.util.ReflectionTestUtils;
+
+import java.io.IOException;
 
 import static br.pucminas.leads.application.support.NotificationSupport.defaultNotification;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 
 @ExtendWith(SpringExtension.class)
-@Import({MailSenderAutoConfiguration.class, SendNotificationEmailAdapter.class, ObjectMapperConfiguration.class})
+@Import({MailSenderAutoConfiguration.class, EmailConfiguration.class, SendNotificationEmailAdapter.class, ObjectMapperConfiguration.class})
 @ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class)
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @ActiveProfiles("test")
 class SendNotificationEmailAdapterTest {
 
-    @InjectMocks
-    private SendNotificationEmailAdapter adapter;
 
-    @MockBean
-    private JavaMailSender mailSender;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    private AutoCloseable autoCloseable;
+    private final SendNotificationEmailAdapter adapter;
+    private SimpleSmtpServer dumbster;
 
     @BeforeEach
-    public void setup() {
-        this.autoCloseable = MockitoAnnotations.openMocks(this);
-        ReflectionTestUtils.setField(this.adapter, "mailSender", mailSender);
-        ReflectionTestUtils.setField(this.adapter, "objectMapper", objectMapper);
+    public void setup() throws IOException {
+        this.dumbster = SimpleSmtpServer.start(5005);
     }
 
     @AfterEach
-    public void tearDown() throws Exception {
-        this.autoCloseable.close();
+    public void tearDown() throws IOException {
+        this.dumbster.close();
     }
 
     @Test
@@ -61,7 +48,6 @@ class SendNotificationEmailAdapterTest {
     public void givenNotificationWhenIsValidThenSendEmail() {
         var notification = defaultNotification().build();
         this.adapter.send(notification);
-        verify(this.mailSender, times(1)).send(any(SimpleMailMessage.class));
     }
 
 }
